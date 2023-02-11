a40af8e0bc9dfc961a44d515949379f7400bebb0
RodrigoAlexisCJ <spikanor9@gmail.com>
2023-02-10 18:20:03 -0600

    login app edited

--- Angular/Login/login/src/app/app.component.ts
+++ Angular/Login/login/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'login';
+  message = 'Welcome to my Angular application';
 }
--- Angular/Login/login/src/app/login/login.component.ts
+++ Angular/Login/login/src/app/login/login.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  username:  string="pashu";
+  password="";
+  constructor(){}
+  ngOnInit(): void {
+    //throw new Error('Method not implemented.');
+  }
+  handleLogin(){
+    console.log(this.username);
+  }
 
 }
